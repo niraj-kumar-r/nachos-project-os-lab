098f57e972f535fd715230bdaa6aaf66fc66710d
Niraj Kumar <nirajk23523@gmail.com>
2023-11-17 18:48:35 +0530

    Dev : tested priority scheduling

--- code/test/multiprogram.c
+++ code/test/multiprogram.c
@@ -7,17 +7,9 @@
 #define stdout 1
 
 int main() {
-    int pid1, pid2;
-    pid1 = Exec("add");
-    pid2 = Exec("help");
-    if (pid1 < 0) {
-        Write("Exec failed: ", 14, stdout);
-        PrintNum(pid1);
-    } else if (pid2 < 0) {
-        Write("Exec failed: ", 14, stdout);
-        PrintNum(pid2);
-    } else {
-        Join(pid1);
-        Join(pid2);
+    while (1) {
+        int pid1, pid2;
+        pid1 = Exec("add");
+        pid2 = Exec("help");
     }
 }
